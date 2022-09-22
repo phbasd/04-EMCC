df14da7438b3cffa25b51356374d2397e3056649
phbasd <qkrgusqls731@naver.com>
2022-09-22 12:57:04 +0900

    item-detail popup finished
    item-detail popup finish

--- script/main.js
+++ script/main.js
@@ -1,3 +1,14 @@
-$(function(){
-	
-})
+
+	$(function() {
+		$("#detail").click(function() {
+			$(".item-detail")//.show("slow"); // ex) 300 : 300ms
+				.removeClass("d-none");	//removeClass() : 클래스 선택자 제거 메소드 vs .addClass()
+		})
+		$("#close").click(function() {
+			$(".item-detail")//.hide("fast");
+				.addClass("d-none");	//addClass() : 클래스 선택자 추가 메소드 vs .
+		})
+	})
+	// $ : 찾아라~
+	// .click(function()(....)) : 클릭하면 처리할 코드
+	// .show(), hide() : effect method
