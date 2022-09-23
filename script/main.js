206e58e2a6bc31d1c7b8a6a4a5ab9014b7c45567
phbasd <qkrgusqls731@naver.com>
2022-09-23 12:50:32 +0900

    script add
    popup - show, hide, class add/remove, attribute get/set

--- script/main.js
+++ script/main.js
@@ -8,6 +8,52 @@
 			$(".item-detail")//.hide("fast");
 				.addClass("d-none");	//addClass() : 클래스 선택자 추가 메소드 vs .
 		})
+		
+		
+		$(function(){
+			$(".small-img").click(function() {
+				$(".")
+			})
+		})
+		
+		
+		
+		const closeBT = $("#close");
+        const detailBT = $("#detail");
+        const itemDIV = $(".item-detail");
+     
+        
+        detailBT.addEventListener("click", function(){
+           itemDIV.classList.remove("d-none");
+        });
+        closeBT.addEventListener("click", function(){
+           itemDIV.classList.add("d-none");
+        });
+		
+		
+		
+		
+		
+		
+		
+		const smallIMG = $(".mall-img li");
+        const img1= $("#img1");
+		
+		smallIMG[0].addEventListener("mouseover", function(){ 
+           img1.setAttribute("src","https://picsum.photos/id/1/2000/2000")
+        })            
+        
+        smallIMG[1].addEventListener("mouseover", function(){
+           img1.setAttribute("src","https://picsum.photos/id/10/2000/2000")
+        })            
+        
+        smallIMG[2].addEventListener("mouseover", function(){
+           img1.setAttribute("src","https://picsum.photos/id/100/2000/2000")
+        })            
+        /* smallIMG.forEach(function(){ 
+           console.log("클릭했습니다")
+        })     */
+		
 	})
 	// $ : 찾아라~
 	// .click(function()(....)) : 클릭하면 처리할 코드
