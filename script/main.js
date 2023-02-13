ba2d30b1e21caf34d462ee12f43abb7db12a05f4
phbasd <qkrgusqls731@naver.com>
2023-02-13 22:53:29 +0900

    2023-02-13
    2023-02-13

--- script/main.js
+++ script/main.js
@@ -1,60 +1,44 @@
+$(function() {
 
-	$(function() {
-		$("#detail").click(function() {
-			$(".item-detail")//.show("slow"); // ex) 300 : 300ms
-				.removeClass("d-none");	//removeClass() : 클래스 선택자 제거 메소드 vs .addClass()
-		})
-		$("#close").click(function() {
-			$(".item-detail")//.hide("fast");
-				.addClass("d-none");	//addClass() : 클래스 선택자 추가 메소드 vs .
-		})
-		
-		
-		$(function(){
-			$(".small-img").click(function() {
-				$(".")
-			})
-		})
-		
-		
-		
-		const closeBT = $("#close");
-        const detailBT = $("#detail");
-        const itemDIV = $(".item-detail");
-     
-        
-        detailBT.addEventListener("click", function(){
-           itemDIV.classList.remove("d-none");
-        });
-        closeBT.addEventListener("click", function(){
-           itemDIV.classList.add("d-none");
-        });
-		
-		
-		
-		
-		
-		
-		
-		const smallIMG = $(".mall-img li");
-        const img1= $("#img1");
-		
-		smallIMG[0].addEventListener("mouseover", function(){ 
-           img1.setAttribute("src","https://picsum.photos/id/1/2000/2000")
-        })            
-        
-        smallIMG[1].addEventListener("mouseover", function(){
-           img1.setAttribute("src","https://picsum.photos/id/10/2000/2000")
-        })            
-        
-        smallIMG[2].addEventListener("mouseover", function(){
-           img1.setAttribute("src","https://picsum.photos/id/100/2000/2000")
-        })            
-        /* smallIMG.forEach(function(){ 
-           console.log("클릭했습니다")
-        })     */
-		
+	const closeBT = $("#close");
+	const openBT = $("#open");
+	const itemDIV = $(".item-detail");
+
+	openBT.click(function() {
+		itemDIV.removeClass("d-none");
+	});
+	closeBT.click(function() {
+		itemDIV.addClass("d-none");
+	});
+
+
+	const smallIMG = $(".small-img li"); // NodeList로 반환
+	const img1 = $("#img1");
+
+	smallIMG[0].mouseenter(function() {  // jQuery : .attr("src"), .attr("src", "value")
+		//console.log(img1.getAttribute("src")); // .getAttribute(), .setAttribute()
+		img1.attr("src", "https://picsum.photos/id/1/2000/2000")
 	})
-	// $ : 찾아라~
-	// .click(function()(....)) : 클릭하면 처리할 코드
-	// .show(), hide() : effect method
+	smallIMG[1].mouseenter(function() {
+		img1.attr("src", "https://picsum.photos/id/10/2000/2000")
+	})
+	smallIMG[2].mouseenter(function() {
+		img1.attr("src", "https://picsum.photos/id/100/2000/2000")
+	})
+
+
+	const toggleSidePanel = $("#closePanel");
+	const sidePanel = $("#sidePanel");
+	const screen = $(".screen")
+
+	toggleSidePanel.click(function() {
+		if (sidePanel.hasClass("collapsed")) {
+			sidePanel.removeClass("collapsed");
+			screen.removeClass("d-none");
+		} else {
+			sidePanel.addClass("collapsed");
+			screen.addClass("d-none");
+		}
+	});
+
+         
